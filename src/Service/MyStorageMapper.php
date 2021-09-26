fd40530d63bb3deb7625feaac4f84275f30ba9c7
Damien Harper <damien.harper@gmail.com>
2021-09-26 11:35:34 +0200

    Multi db & custom mapper

---
+++ src/Service/MyStorageMapper.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service;
+
+use DH\Auditor\Provider\Service\StorageServiceInterface;
+
+class MyStorageMapper
+{
+    // the service expects 2 parameters and should return an object
+    // implementing StorageServiceInterface
+    public static function map(string $entity, array $storageServices): StorageServiceInterface
+    {
+        return $storageServices['dh_auditor.provider.doctrine.storage_services.doctrine.orm.default_entity_manager'];
+    }
+}
